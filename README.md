0b336ce04e8938fd291862842db89a6fb8a2f689
continueBrook <1206507667@qq.com>
2018-12-18 14:42:41 +0800

    v0.0.1

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # vue
-记录关于vue的知识点
+记录关于vue的知识点<br/>
+利用axios实现断点续传,在前台进行控制，onUploadProgress箭头函数获得上传进度
---
+++ src/server.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const multer  = require('multer')
+
+let server = express()
+server.listen(3000)
+
+let multerObj = multer({dest:'./upload'})
+server.use(multerObj.any())
+
+server.use('/api',(req,res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    console.log(req.method);
+    if(req.method != 'OPTIONS'){
+        req.files.forEach((value,key) => {
+            console.log(value);
+            console.log(key);
+        })
+    }
+    res.send('ok');
+})
